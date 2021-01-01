1874428a3e669ac51073cdafe34f45509443748b
VK <viktor@krueckl.de>
2021-01-01 17:40:15 +0100

    update airflow

--- core-container/run.sh
+++ core-container/run.sh
@@ -3,7 +3,11 @@
 case "$1" in
   start)
     python /assure_airflow_db.py
-    airflow initdb
+    airflow db init
+    if [ `airflow users list | grep admin | wc -l` -lt 1 ] 
+    then
+      airflow users create -u admin -p admin -e admin@admin.de -f admin -l admin -r Admin
+    fi
     exec airflow webserver -p 8888 &
     exec airflow scheduler &
     #exec python /setup_routes.py &
